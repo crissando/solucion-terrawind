230a66ae6c96024d4e74aef59811f9968e72cffd
Cristhian Sandoval <crissdavidsando@gmail.com>
2024-02-22 18:48:08 -0500

    fix: corrige error de CORS
    Se añadió la política CORS al pipeline de solicitudes HTTP para solucionar el problema de 'Access-Control-Allow-Origin' que estaba bloqueando las solicitudes desde 'http://localhost:3000'.

--- api-terrawind/Program.cs
+++ api-terrawind/Program.cs
@@ -7,7 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+    .WithOrigins("http://localhost:3000")
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+});
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion")));
 
@@ -16,6 +22,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
